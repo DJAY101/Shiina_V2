fdbc76217655604fbddd45aba7b4becd51aef06a
DJAY101 <55177986+DJAY101@users.noreply.github.com>
2021-02-19 19:12:35 +1100

    Updated Ping cmd

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,15 @@
+
 module.exports = {
 	name: 'ping',
 	description: 'Ping!',
-	execute(message, args) {
-		message.channel.send('Pong.');
+	execute(message, args, client, Discord) {
+		
+		message.channel.send('Loading data').then (async (msg) =>{
+			msg.delete();
+			const embed = new Discord.MessageEmbed()
+			.setColor('#0099ff')
+			.setTitle(`🏓Pong  ${msg.createdTimestamp - message.createdTimestamp}ms.`)
+			message.channel.send(embed);
+		  })
 	},
 };
--- main.js
+++ main.js
@@ -33,7 +33,7 @@ client.on('message', message => {
     if (!client.commands.has(command)) return;
 
     try {
-        client.commands.get(command).execute(message, args);
+        client.commands.get(command).execute(message, args, client, Discord);
     } catch (error) {
         console.error(error);
         message.reply('there was an error trying to execute that command!');
